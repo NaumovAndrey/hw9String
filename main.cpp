11363ef89eaa2c6160ff25d536478dde18871b6c
andrey <naumov.a.n@icloud.com>
2024-02-08 22:29:39 +0700

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "iostream"
+#include <conio.h>
+#include <clocale>
+
+using namespace std;
+
+void task1();
+void task2();
+
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    cout << "Домашняя работа к девятой лекции. Строки и текстовые данные. Базовые алгоритмы со строками" << endl;
+
+
+    while (true)
+    {
+        cout << "1: " << endl;
+        cout << "2: " << endl;
+        cout << "Введите число от 1 до 2 или что-то другое для выхода: ";
+
+        int task;
+        cin >> task;
+
+        if (task == 1) {
+            system("cls");
+            cout << "Задача 1" << endl;
+            task1();
+            _getch();
+            system("cls");
+        }
+
+        else if (task == 2) {
+            system("cls");
+            cout << "Задача 2" << endl;
+            task2();
+            _getch();
+            system("cls");;
+        }
+        else
+        {
+            system("cls");
+            cout << "некорректный ввод. Выход из программы";
+            break;
+        }
+    }
+}
